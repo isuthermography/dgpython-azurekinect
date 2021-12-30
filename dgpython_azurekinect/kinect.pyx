f0620b88bb68a38d46f79946f43f9a247725f815
Steve Holland <sdh4@iastate.edu>
2021-12-30 15:20:34 -0600

    Update to work with latest spatialnde2 API changes

--- dgpython_azurekinect/kinect.pyx
+++ dgpython_azurekinect/kinect.pyx
@@ -815,16 +815,16 @@ class K4A(object,metaclass=dgpy_Module):
             
             cur_acq = self.LowLevel[ResultChan_Name].wait_frame(K4A_WAIT_INFINITE)
             transact = snde.active_transaction(self.recdb)
-            point_cloud_recording = snde.multi_ndarray_recording.create_typed_recording(self.recdb,self.result_channel_ptrs[ResultChan_Name],self,snde.SNDE_RTN_COORD3_INT16)
+            point_cloud_recording_ref = snde.create_recording_ref(self.recdb,self.result_channel_ptrs[ResultChan_Name],self,snde.SNDE_RTN_SNDE_COORD3_INT16)
             transact.end_transaction()
-            point_cloud_recording.rec.metadata = snde.immutable_metadata()
-            point_cloud_recording.rec.mark_metadata_done()
-            point_cloud_recording.allocate_storage([height,width])
+            point_cloud_recording_ref.rec.metadata = snde.immutable_metadata()
+            point_cloud_recording_ref.rec.mark_metadata_done()
+            point_cloud_recording_ref.allocate_storage([height,width])
 
-            cur_acq.get_point_cloud(self.LowLevel[ResultChan_Name],<void *>(<uintptr_t>point_cloud_recording.void_dataptr()), width, height)
-            #cur_acq.get_depth_image(self.LowLevel[ResultChan_Name],<void *>(<uintptr_t>point_cloud_recording.void_dataptr()), width, height)
+            cur_acq.get_point_cloud(self.LowLevel[ResultChan_Name],<void *>(<uintptr_t>point_cloud_recording_ref.void_shifted_arrayptr()), width, height)
+            #cur_acq.get_depth_image(self.LowLevel[ResultChan_Name],<void *>(<uintptr_t>point_cloud_recording_ref.void_shifted_arrayptr()), width, height)
 
-            point_cloud_recording.rec.mark_as_ready()
+            point_cloud_recording_ref.rec.mark_as_ready()
 
             # !!! Need some way to tell this thread to quit when the user exits !!!***
             
